222d1ed8811cf06c701f8bb9375b02f86c868c04
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-04-07 18:17:52 +0600

    bugs fixed

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+//browser
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/' , (req, res) => {
   res.send("Welcome to Red Onion database");
 })
